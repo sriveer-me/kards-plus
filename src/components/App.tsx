284c19ce7f6a72479bc120c864071b38a5f379a0
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2023-11-28 09:39:59 +0530

    feat: integrated carbon library into the project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from '@carbon/react';
 
 export default function App(){
     return(
-        <h1>Hello world from App!</h1>
+        <h1>Hello world from App! <Button>Test</Button></h1>
     );
 }
